def0946ebbdd13065e81e8dca70d18fae8fa88d2
josepablito16 <josepablocif16@gmail.com>
2019-05-23 00:14:28 -0600

    Creacion de pantalla Alarma

--- ionic4-start-theme-master/src/app/app-routing.module.ts
+++ ionic4-start-theme-master/src/app/app-routing.module.ts
@@ -23,7 +23,8 @@ const routes: Routes = [
   { path: 'testhuella9/:myid29/:myid30/:myid31/:myid32/:myid33/:myid34/:myid35/:myid36', loadChildren: './pages/testhuella9/testhuella9.module#Testhuella9PageModule' },
   { path: 'testhuellaresultado/:myid37/:myid38/:myid39/:myid40/:myid41/:myid42/:myid43/:myid44/:myid45', loadChildren: './pages/testhuellaresultado/testhuellaresultado.module#TesthuellaresultadoPageModule' },
   { path: 'calculate-enery/:myLista', loadChildren: './pages/calculate-enery/calculate-enery.module#CalculateEneryPageModule' },
-  { path: 'graphics', loadChildren: './pages/graphics/graphics.module#GraphicsPageModule' },
+  { path: 'graphics', loadChildren: './pages/graphics/graphics.module#GraphicsPageModule' },  { path: 'alarma', loadChildren: './pages/alarma/alarma.module#AlarmaPageModule' },
+
 
 
 ];
