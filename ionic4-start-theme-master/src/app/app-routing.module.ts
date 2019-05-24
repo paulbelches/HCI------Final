18701f0eb57c0189dc6d6c5883614aaf87325691
javiercarpio57 <javierrr.carpio57@gmail.com>
2019-05-23 22:29:48 -0600

    Removiendo archivos y agregando google maps

--- ionic4-start-theme-master/src/app/app-routing.module.ts
+++ ionic4-start-theme-master/src/app/app-routing.module.ts
@@ -8,24 +8,8 @@ const routes: Routes = [
   { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
   { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
   { path: 'edit-profile', loadChildren: './pages/edit-profile/edit-profile.module#EditProfilePageModule' },
-  { path: 'home-results', loadChildren: './pages/home-results/home-results.module#HomeResultsPageModule' },
-  { path: 'home-results/:usuario', loadChildren: './pages/home-results/home-results.module#HomeResultsPageModule' },
-  { path: 'energy', loadChildren: './pages/energy/energy.module#EnergyPageModule' },
-  { path: 'bienvenida-test', loadChildren: './pages/bienvenida-test/bienvenida-test.module#BienvenidaTestPageModule' },
-  { path: 'testhuella1', loadChildren: './pages/testhuella1/testhuella1.module#Testhuella1PageModule' },
-  { path: 'testhuella2/:myid', loadChildren: './pages/testhuella2/testhuella2.module#Testhuella2PageModule' },
-  { path: 'testhuella3/:myid2/:myid3', loadChildren: './pages/testhuella3/testhuella3.module#Testhuella3PageModule' },
-  { path: 'testhuella4/:myid4/:myid5/:myid6', loadChildren: './pages/testhuella4/testhuella4.module#Testhuella4PageModule' },
-  { path: 'testhuella5/:myid7/:myid8/:myid9/:myid10', loadChildren: './pages/testhuella5/testhuella5.module#Testhuella5PageModule' },
-  { path: 'testhuella6/:myid11/:myid12/:myid13/:myid14/:myid15', loadChildren: './pages/testhuella6/testhuella6.module#Testhuella6PageModule' },
-  { path: 'testhuella7/:myid16/:myid17/:myid18/:myid19/:myid20/:myid21', loadChildren: './pages/testhuella7/testhuella7.module#Testhuella7PageModule' },
-  { path: 'testhuella8/:myid22/:myid23/:myid24/:myid25/:myid26/:myid27/:myid28', loadChildren: './pages/testhuella8/testhuella8.module#Testhuella8PageModule' },
-  { path: 'testhuella9/:myid29/:myid30/:myid31/:myid32/:myid33/:myid34/:myid35/:myid36', loadChildren: './pages/testhuella9/testhuella9.module#Testhuella9PageModule' },
-  { path: 'testhuellaresultado/:myid37/:myid38/:myid39/:myid40/:myid41/:myid42/:myid43/:myid44/:myid45', loadChildren: './pages/testhuellaresultado/testhuellaresultado.module#TesthuellaresultadoPageModule' },
-  { path: 'calculate-enery/:myLista', loadChildren: './pages/calculate-enery/calculate-enery.module#CalculateEneryPageModule' },
   { path: 'graphics', loadChildren: './pages/graphics/graphics.module#GraphicsPageModule' },
-
-
+  { path: 'menu-principal', loadChildren: './pages/menu-principal/menu-principal.module#MenuPrincipalPageModule' },
 ];
 
 @NgModule({
---
+++ ionic4-start-theme-master/src/app/pages/menu-principal/menu-principal.page.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LoadingController } from '@ionic/angular';
+import { NativeGeocoder, NativeGeocoderOptions, NativeGeocoderResult } from '@ionic-native/native-geocoder/ngx';
+import { ToastController } from '@ionic/angular';
+
+// import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps';
+
+// import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation/ngx';
+// import { NavController } from '@ionic/angular';
+
+declare var google;
+
+@Component({
+  selector: 'app-menu-principal',
+  templateUrl: './menu-principal.page.html',
+  styleUrls: ['./menu-principal.page.scss'],
+})
+export class MenuPrincipalPage implements OnInit {
+
+  mapRef = null;
+  overlay = null;
+  marker: any;
+
+  // @ViewChild('map') mapElement: ElementRef;
+
+  constructor(
+    private geolocation: Geolocation,
+    private loadCtrl: LoadingController,
+    private nativeGeocoder: NativeGeocoder,
+    public toastController: ToastController
+    ) {
+  }
+
+  ngOnInit(){
+    this.loadMap();
+  }
+
+  async loadMap(){
+    const loading = await this.loadCtrl.create();
+    loading.present();
+
+    const myLatLng = await this.getLocation();
+
+    console.log(myLatLng);
+     
+    const mapEle: HTMLElement = document.getElementById('map');
+
+    this.mapRef  = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+
+    google.maps.event
+    .addListenerOnce(this.mapRef, 'idle', () => {
+      console.log('added');
+      loading.dismiss();
+
+      this.addMarker(myLatLng.lat, myLatLng.lng);
+    })
+
+    google.maps.event
+    .addListener(this.mapRef, 'click', () => {
+      console.log("Hiciste clic en mapa");
+    })
+
+
+
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+  
+    this.nativeGeocoder.reverseGeocode(myLatLng.lat, myLatLng.lng, options)
+      .then((result: NativeGeocoderResult[]) => this.presentToast(JSON.stringify(result[0])))
+      .catch((error: any) => this.presentToast(error));
+    
+    // this.nativeGeocoder.forwardGeocode('Berlin', options)
+    //   .then((coordinates: NativeGeocoderResult[]) => this.presentToast('The coordinates are latitude=' + coordinates[0].latitude + ' and longitude=' + coordinates[0].longitude))
+    //   .catch((error: any) => this.presentToast(error));
+  }
+
+  async presentToast(message: any){
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  private addMarker(lat: number, lng: number){
+    this.marker = new google.maps.Marker({
+      map: this.mapRef,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: { lat, lng }
+    });
+  }
+
+  private async getLocation(){
+    const rta = await this.geolocation.getCurrentPosition()
+    return {
+      lat: rta.coords.latitude,
+      lng: rta.coords.longitude
+    }
+  }
+
+
+}
