bb318f43ab69ce4d03b38d5d502708dea77124a8
javiercarpio57 <javierrr.carpio57@gmail.com>
2019-05-26 21:24:46 -0600

    final de perfil

--- ionic4-start-theme-master/src/app/services/global.service.ts
+++ ionic4-start-theme-master/src/app/services/global.service.ts
@@ -8,6 +8,9 @@ export class GlobalService {
   public idDoc: string;
   public email: string;
   public nombre: string;
+  public contador: number;
+  public primera: boolean;
+
   public dates: any[];
   public paneles: any[];
   public datesEarth: any[];
@@ -17,6 +20,9 @@ export class GlobalService {
     this.email = null;
     this.idDoc = null;
     this.nombre = null;
+    this.primera = true;
+    this.contador = 0;
+    
     this.dates = [];
     this.paneles = [];
     this.datesEarth = [];
